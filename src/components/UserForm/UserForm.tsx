749a1fbaf2cf416ca187626d3c688dca8ce299e2
Alex <morgunowaleks@uandex.ru>
2023-04-07 01:57:47 +0300

    update component userform

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -1,7 +1,19 @@
-import { FC } from "react";
+import { FC, ReactNode, useState } from "react";
 import { useForm, Resolver } from "react-hook-form";
 import { IworkBordersArr } from "../../models/IworkBorders";
 import { workBordersArr } from "../../resources/data";
+import { Select } from "antd";
+import type { SelectProps } from "antd";
+import "./UserForm.css";
+
+const options: SelectProps["options"] = [];
+
+for (let i = 10; i < 36; i++) {
+  options.push({
+    label: i.toString(36) + i,
+    value: i.toString(36) + i,
+  });
+}
 
 type FormValues = {
   username: string;
@@ -11,21 +23,41 @@ type FormValues = {
 };
 
 const UserForm: FC = () => {
+  const [arrRole, setArrRole] = useState(["ANT"]);
+  const [arrWorkBorders, setArrWorkBorders] = useState([
+    workBordersArr[0].name,
+  ]);
+
+  const handleChangeRole = (value: string[]) => {
+    setArrRole(value);
+  };
+
+  const handleChangeWorkBorders = (value: string[]) => {
+    setArrWorkBorders(value);
+  };
+
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
   } = useForm<FormValues>({ mode: "onBlur" });
 
-  const onSubmit = handleSubmit((data) => console.log(data));
+  const onSubmit = handleSubmit((data) => {
+    console.log({ ...data, arrRole, arrWorkBorders });
+  });
 
-  // const workBordersSelect = workBordersArr.map((el) => {
-  //   <option value={el.name}>{el.name}</option>;
-  // });
+  const workBordersSelect: ReactNode[] = workBordersArr.map((el) => {
+    return (
+      <option value={el.name} key={el.id}>
+        {el.name}
+      </option>
+    );
+  });
 
   return (
     <form onSubmit={onSubmit}>
       <input
+        type="text"
         {...register("username", {
           required: "Поле обязательно для заполнения",
           minLength: {
@@ -35,9 +67,12 @@ const UserForm: FC = () => {
         })}
         placeholder="Имя пользователя"
       />
-      {errors?.username && <p>{errors.username.message}</p>}
+      {errors?.username && (
+        <span className="form_errorMessage">{errors.username.message}</span>
+      )}
 
       <input
+        type="text"
         {...register("password", {
           required: "Установите пароль",
           minLength: {
@@ -47,7 +82,9 @@ const UserForm: FC = () => {
         })}
         placeholder="Пароль"
       />
-      {errors?.password && <p>{errors.password.message}</p>}
+      {errors?.password && (
+        <span className="form_errorMessage">{errors.password.message}</span>
+      )}
 
       <input
         {...register("firstName", {
@@ -59,18 +96,47 @@ const UserForm: FC = () => {
         })}
         placeholder="Имя"
       />
-      {errors?.firstName && <p>{errors.firstName.message}</p>}
+      {errors?.firstName && (
+        <span className="form_errorMessage">{errors.firstName.message}</span>
+      )}
 
       <input {...register("lastName")} placeholder="Фамилия" />
 
-      <select name="roles " id="roles " defaultValue={"ANT"}>
-        <option value="ANT">ANT</option>
-        <option value="ANT_MANAGER">ANT_MANAGER</option>
-        <option value="ANT_OFFICER">ANT_OFFICER</option>
-        <option value="DEVELOPER">DEVELOPER</option>
-      </select>
+      <Select
+        mode="multiple"
+        // disabled
+        style={{ width: "100%" }}
+        placeholder="Please select"
+        defaultValue={["ANT"]}
+        onChange={handleChangeRole}
+        options={
+          [
+            { label: "ANT", value: "ANT" },
+            { label: "ANT_MANAGER", value: "ANT_MANAGER" },
+            { label: "ANT_OFFICER", value: "ANT_OFFICER" },
+            { label: "DEVELOPER", value: "DEVELOPER" },
+          ] as SelectProps["options"]
+        }
+      />
 
-      <input type="submit" disabled={!isValid} />
+      <Select
+        mode="multiple"
+        style={{ width: "100%" }}
+        placeholder="Please select"
+        defaultValue={[workBordersArr[0].name]}
+        onChange={handleChangeWorkBorders}
+        options={workBordersArr.map((el) => {
+          return { label: el.name, value: el.name };
+        })}
+      />
+
+      <input
+        type="submit"
+        disabled={
+          arrRole.length === 0 || !isValid || arrWorkBorders.length === 0
+        }
+        className="btn btn-danger"
+      />
     </form>
   );
 };
