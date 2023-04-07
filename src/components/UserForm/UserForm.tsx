c581ac9b38ad3e8ac3d6402a1c257ba354d7f6cf
Alex <morgunowaleks@uandex.ru>
2023-04-07 23:46:52 +0300

    update userform component add popup after send form

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -2,8 +2,11 @@ import { FC, ReactNode, useState } from "react";
 import { useForm, Resolver } from "react-hook-form";
 import { IworkBordersArr } from "../../models/IworkBorders";
 import { workBordersArr } from "../../resources/data";
-import { Select } from "antd";
+import { Select, Modal } from "antd";
 import type { SelectProps } from "antd";
+import { nanoid } from "nanoid";
+import { CheckCircleTwoTone } from "@ant-design/icons";
+import { NavLink } from "react-router-dom";
 import "./UserForm.css";
 
 const options: SelectProps["options"] = [];
@@ -15,6 +18,11 @@ for (let i = 10; i < 36; i++) {
   });
 }
 
+const generateId = () => {
+  const id = nanoid(8);
+  return id;
+};
+
 type FormValues = {
   username: string;
   firstName: string;
@@ -23,6 +31,7 @@ type FormValues = {
 };
 
 const UserForm: FC = () => {
+  const [isSend, setIssend] = useState<boolean>(false);
   const [arrRole, setArrRole] = useState(["ANT"]);
   const [arrWorkBorders, setArrWorkBorders] = useState([
     workBordersArr[0].name,
@@ -39,105 +48,139 @@ const UserForm: FC = () => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors, isValid },
   } = useForm<FormValues>({ mode: "onBlur" });
 
   const onSubmit = handleSubmit((data) => {
-    console.log({ ...data, arrRole, arrWorkBorders });
+    const user = { ...data, arrRole, arrWorkBorders, id: generateId() };
+    console.log(user);
+    reset();
+    setArrRole(["ANT"]);
+    setArrWorkBorders([workBordersArr[0].name]);
+    setIssend(true);
   });
 
-  const workBordersSelect: ReactNode[] = workBordersArr.map((el) => {
-    return (
-      <option value={el.name} key={el.id}>
-        {el.name}
-      </option>
-    );
-  });
+  const handleOk = () => {
+    setIssend(false);
+  };
+
+  const handleCancel = () => {
+    setIssend(false);
+  };
+  // const workBordersSelect: ReactNode[] = workBordersArr.map((el) => {
+  //   return (
+  //     <option value={el.name} key={el.id}>
+  //       {el.name}
+  //     </option>
+  //   );
+  // });
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        {...register("username", {
-          required: "Поле обязательно для заполнения",
-          minLength: {
-            value: 3,
-            message: "Минимально 3 символов",
-          },
-        })}
-        placeholder="Имя пользователя"
-      />
-      {errors?.username && (
-        <span className="form_errorMessage">{errors.username.message}</span>
-      )}
+    <>
+      <Modal
+        title="Спасибо за обращение!"
+        open={isSend}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        style={{ textAlign: "center" }}
+      >
+        <CheckCircleTwoTone style={{ fontSize: "50px" }} />
+        <p>Данные отправлены</p>
+        <NavLink to="/">На главную</NavLink>
+      </Modal>
 
-      <input
-        type="text"
-        {...register("password", {
-          required: "Установите пароль",
-          minLength: {
-            value: 4,
-            message: "Минимально 4 символа",
-          },
-        })}
-        placeholder="Пароль"
-      />
-      {errors?.password && (
-        <span className="form_errorMessage">{errors.password.message}</span>
-      )}
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          {...register("username", {
+            required: "Поле обязательно для заполнения",
+            minLength: {
+              value: 3,
+              message: "Минимально 3 символов",
+            },
+          })}
+          placeholder="Имя пользователя"
+        />
+        {errors?.username && (
+          <span className="form_errorMessage">{errors.username.message}</span>
+        )}
 
-      <input
-        {...register("firstName", {
-          required: "Поле обязательно для заполнения",
-          minLength: {
-            value: 2,
-            message: "Минимально 2 символа",
-          },
-        })}
-        placeholder="Имя"
-      />
-      {errors?.firstName && (
-        <span className="form_errorMessage">{errors.firstName.message}</span>
-      )}
+        <input
+          type="text"
+          {...register("password", {
+            required: "Установите пароль",
+            minLength: {
+              value: 4,
+              message: "Минимально 4 символа",
+            },
+          })}
+          placeholder="Пароль"
+        />
+        {errors?.password && (
+          <span className="form_errorMessage">{errors.password.message}</span>
+        )}
 
-      <input {...register("lastName")} placeholder="Фамилия" />
+        <input
+          {...register("firstName", {
+            required: "Поле обязательно для заполнения",
+            minLength: {
+              value: 2,
+              message: "Минимально 2 символа",
+            },
+          })}
+          placeholder="Имя"
+        />
+        {errors?.firstName && (
+          <span className="form_errorMessage">{errors.firstName.message}</span>
+        )}
 
-      <Select
-        mode="multiple"
-        // disabled
-        style={{ width: "100%" }}
-        placeholder="Please select"
-        defaultValue={["ANT"]}
-        onChange={handleChangeRole}
-        options={
-          [
-            { label: "ANT", value: "ANT" },
-            { label: "ANT_MANAGER", value: "ANT_MANAGER" },
-            { label: "ANT_OFFICER", value: "ANT_OFFICER" },
-            { label: "DEVELOPER", value: "DEVELOPER" },
-          ] as SelectProps["options"]
-        }
-      />
+        <input {...register("lastName")} placeholder="Фамилия" />
 
-      <Select
-        mode="multiple"
-        style={{ width: "100%" }}
-        placeholder="Please select"
-        defaultValue={[workBordersArr[0].name]}
-        onChange={handleChangeWorkBorders}
-        options={workBordersArr.map((el) => {
-          return { label: el.name, value: el.name };
-        })}
-      />
+        <Select
+          mode="multiple"
+          style={{ width: "100%" }}
+          placeholder="Please select"
+          // defaultValue={["ANT"]}
+          value={arrRole}
+          onChange={handleChangeRole}
+          options={
+            [
+              { label: "ANT", value: "ANT" },
+              { label: "ANT_MANAGER", value: "ANT_MANAGER" },
+              { label: "ANT_OFFICER", value: "ANT_OFFICER" },
+              { label: "DEVELOPER", value: "DEVELOPER" },
+            ] as SelectProps["options"]
+          }
+        />
+        {!arrRole.length ? (
+          <span className="form_errorMessage">Выберите значение</span>
+        ) : null}
 
-      <input
-        type="submit"
-        disabled={
-          arrRole.length === 0 || !isValid || arrWorkBorders.length === 0
-        }
-        className="btn btn-danger"
-      />
-    </form>
+        <Select
+          mode="multiple"
+          style={{ width: "100%" }}
+          placeholder="Please select"
+          // defaultValue={[workBordersArr[0].name]}
+          value={arrWorkBorders}
+          onChange={handleChangeWorkBorders}
+          options={workBordersArr.map((el) => {
+            return { label: el.name, value: el.name };
+          })}
+        />
+        {!arrWorkBorders.length ? (
+          <span className="form_errorMessage">Выберите значение</span>
+        ) : null}
+
+        <input
+          type="submit"
+          disabled={
+            arrRole.length === 0 || !isValid || arrWorkBorders.length === 0
+          }
+          className="btn btn-danger"
+        />
+      </form>
+    </>
   );
 };
 
